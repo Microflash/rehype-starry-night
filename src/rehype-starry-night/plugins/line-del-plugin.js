2ec7b119c40eb90d118edefdd698a6ac1c88ee45
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2024-07-05 22:35:55 +0530

    feat: line del plugin

---
+++ src/rehype-starry-night/plugins/line-del-plugin.js
@@ -0,0 +1,20 @@
+function pluginOptions(globalOptions) {
+	return globalOptions?.metadata?.del || [];
+}
+
+function plugin(globalOptions, lines) {
+	const del = pluginOptions(globalOptions);
+	if (del.length > 0) {
+		del.forEach(lineNumber => {
+			if (lines.has(lineNumber)) {
+				const line = lines.get(lineNumber);
+				line.properties["data-line-removed"] = "";
+			}
+		})
+	}
+}
+
+export default {
+	type: "line",
+	plugin
+}
