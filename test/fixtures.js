89461f6ad89db4c1262f094b38085428529133eb
Naiyer <19614213+naiyerasif@users.noreply.github.com>
2025-12-13 18:38:12 +0530

    test: skipping a language

--- test/fixtures.js
+++ test/fixtures.js
@@ -104,6 +104,22 @@ x -> y
 `
 	},
 	{
+		title: "codeblock with skipped language",
+		options: {
+			plainText: ["mermaid"]
+		},
+		input: `
+\`\`\`mermaid
+flowchart TD
+	A[Christmas] -->|Get money| B(Go shopping)
+	B --> C{Let me think}
+	C -->|One| D[Laptop]
+	C -->|Two| E[iPhone]
+	C -->|Three| F[fa:fa-car Car]
+\`\`\`
+`
+	},
+	{
 		title: "codeblock rendered with custom namespace",
 		options: {
 			namespace: "highlight"
